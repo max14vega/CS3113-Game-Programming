b09d27b79b02d2a80b18f970066ea21130a76a68
Maximiliano Vega <71275115+max14vega@users.noreply.github.com>
2024-04-14 22:11:45 -0400

    Final Platformer Update

--- Platformer/Platformer/main.cpp
+++ Platformer/Platformer/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Max Vega
-* Assignment: Rise of the AI
-* Date due: 2024-03-30, 11:59pm
+* Assignment: Platformer
+* Date due: 2024-04-13, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
